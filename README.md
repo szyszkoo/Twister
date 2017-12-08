5842ec498732c41ceac0eb89427516925b6acf08
W <wojctrz@gmail.com>
2017-12-08 13:26:17 +0100

    some changes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Twister
-Arduino based automatic pet feeder
+# Twister 9000!
+Arduino based automatic pet feeder!!
+
+DS1302 library by Matt Sparks ([GitHub](https://github.com/msparks/arduino-ds1302.git))  
+Arduino-IRremote library by Ken Shirriff ([GitHub](https://github.com/z3t0/Arduino-IRremote.git)) used under the terms of the GNU LGPL 2.1 license
---
+++ Twister9000/Twister9000.ino
@@ -0,0 +1,22 @@
+#include <DS1302.h> //Clock
+#include <IRremote.h> //IR sensor
+#include <Servo.h>
+
+#define SERVO 9   //digital PWM servo pin
+#define SERVO_ENABLE 11
+#define CLK_RST 2 //three real time clock pins
+#define CLK_IO 3
+#define CLK_SCLK 4
+#define IR 5      //IR sensor pin
+#define WS A0     //water sensor analog in
+#define PUMP 10   //water pump pin
+
+void setup()
+{
+  
+}
+
+void loop()
+{
+ 
+}
